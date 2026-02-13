c20666ecd8ea20567998bde97dc1884c6efc857a
Bilal Rizwan <bilal.rizwaan1909@gmail.com>
2026-02-13 06:13:57 +0500

    Complete Home Page

---
+++ includes/header.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Home Page</title>
+    <link rel="shortcut icon" type="image/x-icon" href="images/icons/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="images/icons/apple-touch-icon.png">
+    <link rel="icon" href="images/icons/favicon-16x16.png" sizes="16x16" />
+    <link rel="icon" href="images/icons/favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" href="images/icons/android-chrome-192x192.png" sizes="192x192" />
+    <link rel="icon" href="images/icons/android-chrome-512x512.png" sizes="512x512" />
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta name=" author" content="" />
+    <meta property="og:type" content="website" />
+    <meta property="og:image" content="images/logo/logo.png" />
+    <meta property="og:url" content="" />
+    <meta property="og:description" content="" />
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" />
+    <link rel="stylesheet" href="css/owl.carousel.min.css" />
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/media.css" />
+</head>
+
+<body>
+    <div class="wrapper">
+        <header>
+            <div class="container">
+                <div class="nav">
+                    <div class="logo">
+                        <a href="./"><img src="images/logo/logo.png" alt="logo" /></a>
+                    </div>
+                    <nav>
+                        <div class="nav-menu">
+                            <ul class="nav-links">
+                                <li><a href="#how-works" class="nav-link ">How It Works</a></li>
+                                <li><a href="#features" class="nav-link">Features</a></li>
+                                <li><a href="#pricing" class="nav-link">Pricing</a></li>
+                                <li>
+                                    <a href="void:;" class="nav-link">Blog</a>
+                                </li>
+                                <li><a href="void:;" class="nav-link">Contact</a></li>
+                                <li><a href="void:;" class="nav-link">About</a></li>
+                                <li><a href="#" class="nav-link login" data-bs-toggle="modal"
+                                        data-bs-target="#login">Login</a></li>
+                                <li class="btn-sign-up"><a href=" " data-bs-toggle="modal"
+                                        data-bs-target="#existingUser">Sign Up</a></li>
+                            </ul>
+                        </div>
+                        <div class="nav-mobile-menu">
+                            <span class="line line1"></span>
+                            <span class="line line2"></span>
+                            <span class="line line3"></span>
+                        </div>
+                    </nav>
+                    <div class="nav-button">
+                        <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#existingUser">Sign
+                            Up</button>
+                    </div>
+                </div>
+            </div>
+        </header>
